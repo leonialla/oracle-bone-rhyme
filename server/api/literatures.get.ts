67aae36f3f059ecd415f9ebe073fb76d77dbbd41
leonialla <lswx0925@gmail.com>
2024-02-06 02:37:53 +0800

    revert: fallback to default prisma client location

--- server/api/literatures.get.ts
+++ server/api/literatures.get.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '~/prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 export default defineEventHandler(async (event) => {
   const prisma = new PrismaClient()
