792c65ca8b6dc604e8e94d6b25ceca3c484cfa6e
leonialla <lswx0925@gmail.com>
2024-04-16 21:43:19 +0800

    feat: welcome screen

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,27 @@
+<script setup lang="ts">
+const atHomePage = ref<boolean>(true)
+</script>
+
 <template>
-  <ClientOnly>
-    <Plum />
-  </ClientOnly>
-  <div v-if="!isMobileDevice" h="screen" flex="~ col">
-    <div px="30" py="4">
-      <TheNav />
-      <div py="2" flex="auto">
-        <NuxtPage :keepalive="true" />
+  <div v-if="atHomePage" w="screen" h="screen" @click="atHomePage = false">
+    <img src="/welcome.png">
+  </div>
+  <div v-else>
+    <ClientOnly>
+      <Plum />
+    </ClientOnly>
+    <div v-if="!isMobileDevice" h="screen" flex="~ col">
+      <div px="30" py="4">
+        <TheNav />
+        <div py="2" flex="auto">
+          <NuxtPage :keepalive="true" />
+        </div>
       </div>
     </div>
-  </div>
-  <div v-else h="screen" flex="~" justify="center" items="center">
-    <div>
-      此应用目前暂不支持移动端设备，请使用 PC 端访问。
+    <div v-else h="screen" flex="~" justify="center" items="center">
+      <div>
+        此应用目前暂不支持移动端设备，请使用 PC 端访问。
+      </div>
     </div>
   </div>
 </template>
